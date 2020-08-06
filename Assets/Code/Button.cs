e9cea3b799ef7abb4c0fc59f251e28637982021f
Kevin Wall <kwall@sci.utah.edu>
2020-08-06 17:55:33 -0600

    Button.cs, CloseButton.cs
    Added a new superclass for buttons. It allows the definition of special sprites and colors for the touched and pressed states. It also provides an abstract method OnButtonUp() that allows subclasses to implement behavior.
    
    Added a subclass, CloseWindowButton, that closes the window it appears on.

---
+++ Assets/Code/Button.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public abstract class Button : UIElement
+{
+    Sprite rest_sprite;
+    Color rest_color;
+
+    public Image Image;
+    public Sprite TouchSprite, DownSprite;
+    public Color TouchColor = Color.white, 
+                 DownColor = Color.white;
+
+    protected virtual void Start()
+    {
+        rest_sprite = Image.sprite;
+        rest_color = Image.color;
+
+        if (TouchSprite == null)
+            TouchSprite = rest_sprite;
+
+        if (DownSprite == null)
+            DownSprite = TouchSprite;
+    }
+
+    protected virtual void Update()
+    {
+        if (IsTouched)
+        {
+            if (InputUtility.IsMouseLeftPressed)
+            {
+                Image.sprite = DownSprite;
+                Image.color = DownColor;
+            }
+            else
+            {
+                Image.sprite = TouchSprite;
+                Image.color = TouchColor;
+            }
+
+            if (InputUtility.WasMouseLeftReleased)
+                OnButtonUp();
+        }
+        else
+        {
+            Image.sprite = rest_sprite;
+            Image.color = rest_color;
+        }
+    }
+
+    protected abstract void OnButtonUp();
+}
---
+++ Assets/Code/CloseWindowButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseWindowButton : Button
+{
+    public Window Window;
+
+    protected override void OnButtonUp()
+    {
+        Window.IsOpen = false;
+    }
+}
