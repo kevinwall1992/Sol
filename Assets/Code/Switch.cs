d4bc98484281fa6e7b48f5a3bd5c3cd7fbeaa9e8
Kevin Wall <kwall@sci.utah.edu>
2020-09-19 16:02:54 -0600

    Switch
    Implements a on-off button using a two-position knob skeuomorph.

---
+++ Assets/Code/Switch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteAlways]
+public class Switch : Button
+{
+    public Sprite OffSprite, OnSprite, OnTouchSprite;
+
+    public bool IsOn = false;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        rest_sprite = OffSprite;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if(IsOn && !IsDown)
+        {
+            if (IsPointedAt)
+                Image.sprite = OnTouchSprite;
+            else
+                Image.sprite = OnSprite;
+        }
+    }
+
+    protected override void OnButtonUp()
+    {
+        IsOn = !IsOn;
+    }
+}
