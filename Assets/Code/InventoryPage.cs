0822f188b3a458699e47110f1d685ccb16c30d73
Kevin Wall <kwall@sci.utah.edu>
2020-09-19 16:32:20 -0600

    InventoryPage now actually a Page

--- Assets/Code/InventoryPage.cs
+++ Assets/Code/InventoryPage.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class InventoryPage : MonoBehaviour
+public class InventoryPage : Page
 {
     public Grid ElementGrid;
     public Switch IconSwitch;
