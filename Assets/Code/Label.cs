72c8c2d4b13687f9d9d09d9b35372042826823cd
Kevin Wall <kwall@sci.utah.edu>
2020-09-19 15:54:04 -0600

    Label
    Labels are text with a background image that scales to text length.

---
+++ Assets/Code/Label.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class Label : UIElement
+{
+    public TMPro.TextMeshProUGUI Text;
+    public Image Background;
+    public Color TextColor, BackgroundColor;
+
+    public bool AutomaticWidth = false;
+
+    private void Update()
+    {
+        Text.color = TextColor;
+        Background.color = BackgroundColor;
+
+        int text_width = Text.text.Length * 6;
+
+        RectTransform background_transform = Background.transform as RectTransform;
+        background_transform.sizeDelta =  
+            background_transform.sizeDelta.XChangedTo(text_width + 1);
+
+        if (AutomaticWidth)
+        {
+            RectTransform.sizeDelta = background_transform.sizeDelta;
+            Text.overflowMode = TMPro.TextOverflowModes.Overflow;
+        }
+    }
+}
