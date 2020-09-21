ad5456c932b1c53f948d24b1ce2d2cdc931dc088
Kevin Wall <kwall@sci.utah.edu>
2020-09-21 12:27:49 -0600

    More MoveToFront scripts

---
+++ Assets/Code/MoveToFrontWhenClicked.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToFrontWhenClicked : UIElement
+{
+    private void Update()
+    {
+        if (IsTouched && InputUtility.WasMouseLeftReleased)
+            transform.SetAsLastSibling();
+    }
+}
---
+++ Assets/Code/MoveToFrontWhenPressed.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToFrontWhenPressed : UIElement
+{
+    private void Update()
+    {
+        if (IsTouched && InputUtility.IsMouseLeftPressed)
+            transform.SetAsLastSibling();
+    }
+}
