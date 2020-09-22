3a9307e65261655cdd865dcb221572974870a3b6
Kevin Wall <kwall@sci.utah.edu>
2020-09-22 16:53:15 -0600

    AllOrNothingText
    This is a simple script for making text hide if theres not enough space to display every character.

---
+++ Assets/Code/AllOrNothingText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TMPro.TextMeshProUGUI))]
+public class AllOrNothingText : UIElement
+{
+    Color rest_color;
+
+    public int CharacterWidth = 6;
+    public int Margin = 1;
+
+    public TMPro.TextMeshProUGUI Text
+    { get { return GetComponent<TMPro.TextMeshProUGUI>(); } }
+
+    private void Start()
+    {
+        rest_color = Text.color;
+    }
+
+    private void Update()
+    {
+        if ((Text.text.Length * CharacterWidth + Margin) > 
+            RectTransform.rect.width)
+            Text.color = Color.clear;
+        else
+            Text.color = rest_color;
+    }
+}
