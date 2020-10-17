cc87b6851d17e26af159558105a808214e93f4de
Kevin Wall <kwall@sci.utah.edu>
2020-10-17 09:56:06 -0600

    Room.cs
    Room is a part of a Station that will eventually be used to operate machines, store goods, or house people.
    
    Right now its just a stub ItemContainer that can only store solids.

---
+++ Assets/Code/Room.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteAlways]
+[RequireComponent(typeof(SolidItemContainer))]
+public class Room : Craft.Part
+{
+    public ItemContainer Container
+    { get { return GetComponent<ItemContainer>(); } }
+}
---
+++ Assets/Code/SolidItemContainer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolidItemContainer : ItemContainer.Script,
+                                  ItemContainer.Filter
+{
+    public bool IsStorable(Item item)
+    {
+        return item.IsPhysical() && item.IsSolid();
+    }
+}
